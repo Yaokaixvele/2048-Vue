39b6487c2a9e7cc9a58302169301e29a16910b43
ccc <16682092336@163.com>
2026-04-05 22:12:51 +0800

    // 获取浏览器宽度  注意：ref是在windowWith变后视图变，而非让windowWith和window.innerWIdth一样，所以需要设立方法然后绑定事件

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,21 @@
 <script setup lang="ts">
+import { onMounted, onUnmounted, ref } from 'vue';
+
+// 获取浏览器宽度  注意：ref是在windowWith变后视图变，而非让windowWith和window.innerWIdth一样，所以需要设立方法然后绑定事件
+const windowWith = ref(window.innerWidth)
+const updateWith = () =>{
+  windowWith.value = window.innerWidth
+}
+onMounted(() => {
+  document.addEventListener('resize',updateWith)
+})
+onUnmounted(() => {
+  document.removeEventListener('resize',updateWith)
+})
 </script>
 
 <template>
   <div>
-    123
+    
   </div>
 </template>
